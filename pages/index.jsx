fc08c8dfd6315ff3bc32468802716e18d365f7a3
Lpcsouza <17843059+lpcsouza@users.noreply.github.com>
2024-11-12 22:07:37 -0300

    criado componente de post.

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,8 +2,33 @@ import styled from "styled-components"
 import Navbar from "../src/components/layout/Navbar"
 import Container from "../src/components/layout/Container"
 import CreatePost from "../src/components/cards/CreatePost"
+import H3 from "../src/components/typography/H3"
+import Post from "../src/components/cards/Post"
 
-const Content = styled.div``
+const Content = styled.div`
+  margin: 50px 0;
+`
+
+const LastPostText = styled(H3)`
+  padding: 40px 0;
+`
+
+const RefreshPosts = styled.span`
+  font-weight: bold;
+  color: ${(props) => props.theme.primary};
+  cursor: pointer;
+`
+
+const RefreshPostsContainer = styled.div`
+  text-align: center;
+`
+
+const PostContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  margin-top: 20px;
+`
 
 function HomePage() {
   return (
@@ -11,7 +36,16 @@ function HomePage() {
       <Navbar />
       <Content>
         <Container>
-          <CreatePost></CreatePost>
+          <CreatePost />
+          <LastPostText>Latest posts:</LastPostText>
+          <RefreshPostsContainer>
+            <RefreshPosts>Load new posts</RefreshPosts>
+          </RefreshPostsContainer>
+          <PostContainer>
+            <Post />
+            <Post />
+            <Post />
+          </PostContainer>
         </Container>
       </Content>
     </>
